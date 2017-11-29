1cb38534d449a3d15f6adab16cecc122b6bd340e
moltenshark <alex@berez.com>
2017-11-28 20:46:39 -0600

    minor fixes

--- hangman.js
+++ hangman.js
@@ -10,23 +10,30 @@ function startGame() {
 }
 
 function displayGame(guess) {
-	let displayArr = [], newRight = 0;
+	let displayArr = [], nowRight = 0;
 	for (let letter of letterArr) {
 		let match = false;
 		for (let guess of guessArr) if (letter.visible == guess) match = true;
-		match ? (displayArr.push(letter.visible), newRight++) : displayArr.push(letter.hidden);
+		match ? (displayArr.push(letter.visible), nowRight++) : displayArr.push(letter.hidden);
 	}
-	if (guess && newRight > totalRight) {
+	if (guess && nowRight > totalRight) {
 		console.log(`\nCorrect!`);
-		totalRight = newRight;
+		totalRight = nowRight;
 		console.log(`\n${displayArr.join(" ")}\n`);
 	}
 	else if (guess) {
 		wrongLeft--;
 		if (wrongLeft > 1) console.log(`\nIncorrect! ${wrongLeft} guesses remaining!\n`);
-		else if (wrongLeft > 0) console.log(`\nIncorrect! ${wrongLeft} guess remaining!\n`);
+		else if (wrongLeft) console.log(`\nIncorrect! ${wrongLeft} guess remaining!\n`);
 	}
 	else console.log(`\n${displayArr.join(" ")}\n`);
+}
+
+function validateNew(letter) {
+	if (letter == "exit") return true;
+	let noMatch = true;
+	for (let guess of guessArr) if (letter == guess) noMatch = false;
+	return (letter.match(/^[A-Za-z]+$/) && letter.length == 1) ? noMatch || "You've already guessed that letter!" : false || "Invalid guess!";
 }
 
 function userPrompt() {
@@ -45,12 +52,5 @@ function userPrompt() {
 }
 
 function playAgain() { inquirer.prompt([{ type: "confirm", message: "Play again?", name: "replay" }]).then((response)=>{ if (response.replay) startGame(); }); }
-
-function validateNew(letter) {
-	if (letter == "exit") return true;
-	let noMatch = true;
-	for (let guess of guessArr) if (letter == guess) noMatch = false;
-	return (letter.match(/^[A-Za-z]+$/) && letter.length == 1) ? noMatch || "You've already guessed that letter!" : false || "Invalid guess!";
-}
 
 startGame();
